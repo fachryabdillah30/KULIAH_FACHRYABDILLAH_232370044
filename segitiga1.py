f6a8d31b2163789e758dec8042238e37d98f80ac
fachryabdillah30 <fachryabdillah7078@gmail.com>
2025-11-16 17:40:33 +0700

    Add files via upload

---
+++ segitiga1.py
@@ -0,0 +1,12 @@
+string = ""
+bar = 1
+x = int(input("masukkkan angka : "))
+while bar <= x:
+  kol = bar
+  while kol  > 0:
+    string = string + " * "
+    kol = kol - 1
+  string = string + "\n"
+  bar = bar + 1
+  
+print (string)
